766e01cdea753bdced44e89911958c5afa76e066
bluke7ide <luisfernandoamey@gmail.com>
2024-10-21 17:33:26 -0600

    fix: termina el codigo
    aun no implementado

--- ejercicio4.m
+++ ejercicio4.m
@@ -1,13 +1,18 @@
 % ejercicio 4
-function c = calculo_aprox(f, n)
+function c = calculo_aprox(f, n, inf, sup)
     M = zeros(n);
+    b = zeros(n);
     for i = 1:n
+        b(i) = integral(@(x) f*x^(i-1), inf, sup);
         for j = 1:n
             M(i,j) = 1/(i+j+1);
         end
     end
-
     
+    % M tiene rango completo, procedemos por pseudo-inversa
+    inversa = (M'\M)*M';
+    c = inversa*b;
+
 end
 
 f = @(x) cos(4.*pi.*x);
