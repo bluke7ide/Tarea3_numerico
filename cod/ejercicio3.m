9effebd636e051945742d55f89e70f036df00589
bluke7ide <luisfernandoamey@gmail.com>
2024-11-04 09:20:36 -0600

    checkpoint: se me había olvidado en subir

--- cod/ejercicio3.m
+++ cod/ejercicio3.m
@@ -5,9 +5,9 @@ y = linspace(0,2,200);
 [xx,yy] = meshgrid(x,y);
 
 f = @(x,y) sin(2.*pi.*(x+y)).*sin(pi.*(x-y));
-surf(xx,yy,f(xx,yy))
-view([-45 45])
-shading interp
+% surf(xx,yy,f(xx,yy))
+% view([-45 45])
+% shading interp
 
 zz = f(xx,yy);
 [U,S,V] = svd(zz);
@@ -18,26 +18,30 @@ for i = 2:4
     aprox{i} = aprox{i-1} + val(i)*U(:,i)*V(:,i)';
 end
 
-figure(1)
-for i = 1:4
-    subplot(2,3,i);
-    surf(xx, yy, aprox{i})
-    shading interp
-end
+% figure(1)
+% for i = 1:4
+%     subplot(2,3,i);
+%     surf(xx, yy, aprox{i})
+%     shading interp
+% end
+% 
+% subplot(2,3,5)
+% surf(xx,yy,f(xx,yy))
+% shading interp
+% 
+% error = cell(4,1);
+% for i = 1:4
+%     error{i} = abs(f(xx, yy) - aprox{i});
+%     figure(2)
+%     subplot(2, 2, i)
+%     surf(xx, yy, error{i})
+%     shading flat
+% end
 
-subplot(2,3,5)
-surf(xx,yy,f(xx,yy))
-shading interp
-
-error = cell(4,1);
-for i = 1:4
-    error{i} = abs(f(xx, yy) - aprox{i});
-    figure(2)
-    subplot(2, 2, i)
-    surf(xx, yy, error{i})
-    shading flat
-end
-
-disp(val)
+disp(val(1:4))
 rank(zz)
-    
+   
+disp(norm(cos(2.*pi.*x).*cos(pi.*x)) * norm(sin(2.*pi.*y).*sin(pi.*y)))
+disp(norm(sin(2.*pi.*x).*sin(pi.*x)) * norm(cos(2.*pi.*y).*cos(pi.*y)))
+disp(norm(sin(2.*pi.*x).*cos(pi.*x)) * norm(cos(2.*pi.*y).*sin(pi.*y)))
+disp(norm(cos(2.*pi.*x).*sin(pi.*x)) * norm(sin(2.*pi.*y).*cos(pi.*y)))
--- cod/ejercicio4.m
+++ cod/ejercicio4.m
@@ -18,13 +18,37 @@ end
 f = @(x) cos(4.*pi.*x);
 c = calculo_aprox(f, 10, 0, 1);
 p = @(x) polyval(c(end:-1:1), x);
-integral(@(x) f(x) - p(x), 0, 1)
+integral(@(x) (f(x) - p(x)).^2, 0, 1)
 
 xx = linspace(0,1);
 plot(xx, f(xx), '-')
 hold on
-plot(xx, p(xx), 'b-')
+plot(xx, p(xx), 'r--')
 hold off
 
+function p = aprox_leg(n, f)
+    pn = cell(n, 1);
+    pn{1} = @(x) 1;
+    pn{2} = @(x) x;
+    for k = 2:(n-1)
+        pn{k+1} = @(x) ((2.*k+1).*x.*pn{k}(x) - k.*pn{k-1}(x))./(k+1);
+    end
+    betas = zeros(n, 1);
+    for k = 1:n
+        betas(k) = integral(@(x)f(x).*pn{k}(x), 0, 1)./...
+            integral(@(x)pn{k}(x).^2, 0, 1, "ArrayValued",true);
+    end
+
+    p = @(x) sum(cell2mat(arrayfun(@(k) betas(k) .* pn{k}(x),...
+        1:n, UniformOutput=false)));
+
+end
 
 
+p = aprox_leg(10, f);
+
+figure(2)
+plot(xx, f(xx), '-')
+hold on
+plot(xx, arrayfun(p, xx), 'r--')
+hold off
