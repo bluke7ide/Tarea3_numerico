78c3a9c47ada8bc81e376691dbbc796d77ba7b73
bluke7ide <luisfernandoamey@gmail.com>
2024-11-08 23:14:41 -0600

    style: graficos y displays

--- cod/ejercicio4.m
+++ cod/ejercicio4.m
@@ -1,24 +1,32 @@
-% ejercicio 4
-function c = calculo_aprox(f, n, inf, sup)
+function c = calculo_aprox(f, n)
+    %
+    %   Hace un cálculo aproximado de la función aproximada con 
+    %   la matriz de Hilbert
+    %
+    %   Inputs
+    %       f: función a aproximar 
+    %       n: tamaño del polinomio interpolador
+    %
+    %   Outputs
+    %       c: vector de constantes de la factorización Mc = b
+
     M = zeros(n);
     b = zeros(n,1);
     for i = 1:n
-        b(i) = integral(@(x) f(x).*x.^(i-1), inf, sup);
+        b(i) = integral(@(x) f(x).*x.^(i-1), 0, 1);
         for j = 1:n
             M(i,j) = 1/(i+j-1);
         end
     end
-    
     c = M\b;
-
 end
 
 f = @(x) cos(4.*pi.*x);
-c = calculo_aprox(f, 10, 0, 1);
+c = calculo_aprox(f, 10);
 p = @(x) polyval(c(end:-1:1), x);
 integral(@(x) (f(x) - p(x)).^2, 0, 1)
-
 xx = linspace(0,1); 
+figure;
 plot(xx, f(xx), '-', 'DisplayName', 'f(x) = cos(4\pi x)')
 hold on
 plot(xx, p(xx), 'r--', 'DisplayName', 'Aproximación p(x)')
@@ -29,19 +37,34 @@ xlabel('x');
 ylabel('Valor de la función');
 
 function p = aprox_leg(n, f)
+    %
+    %   Hace un cálculo aproximado de la función aproximada con 
+    %   los polinomios de legendre
+    %
+    %   Inputs
+    %       f: función a aproximar 
+    %       n: tamaño del polinomio interpolador
+    %
+    %   Outputs
+    %       p: polinomio interpolador 
+
     pn = cell(n, 1);
     pn{1} = @(x) 1; 
     pn{2} = @(x) x; 
     
+    % Construye los polinomios de Legendre
     for k = 2:(n-1)
         pn{k+1} = @(x) ((2*k-1)*x.*pn{k}(x) - (k-1)*pn{k-1}(x)) / (k);
     end
-  
+    
+    % Calcula los betas
     betas = zeros(n, 1);
     for k = 1:n
         betas(k) = integral(@(x) f(x) .* pn{k}(2*x-1), 0, 1) / ...
                    integral(@(x) pn{k}(2*x-1).^2, 0, 1, 'ArrayValued', true);
     end
+
+    % suma para obtener el polinomio
     p = @(x) sum(cell2mat(arrayfun(@(k) betas(k) .* pn{k}(2*x-1), ...
         1:n, 'UniformOutput', false)));
 end
@@ -49,7 +72,7 @@ end
 n = 10;
 p = aprox_leg(n, f);
 
-figure(2)
+figure;
 xx = linspace(0, 1, 100); 
 plot(xx, f(xx), '-', 'DisplayName', 'f(x) = cos(4\pi x)'); 
 hold on;
--- cod/ejercicio8.m
+++ cod/ejercicio8.m
@@ -1,31 +1,54 @@
-% ejercicio 8
-
 function sigmas = calculo_s2(xn, yn)
+    %
+    %   Calcula los sigmas para poder realizar s2
+    %
+    %   Inputs
+    %       xn: coordenadas x de los puntos a interpolar
+    %       yn: coordenadas y de los puntos a interpolar
+    %
+    %   Outputs
+    %       sigmas: coeficientes de la función s2 
+
     m = length(xn);
     x = zeros(m);
     y = zeros(m,1);
     for i = 1:(m-1)
+        % diferencias de xn
         x(i,i) = (xn(i+1)-xn(i))/2;
         x(i,i+1) = x(i,i);
+        % diferencias de yn
         y(i) = yn(i+1) - yn(i);
     end
+    % condición de sigmas, que el último debe de ser igual al primero
     x(end, 1) = 1;
     x(end, end) = -1;
     y(end) = 0;
 
     sigmas = x\y;
-    
 end
 
 m = 15;
 xn = sort(rand(m+1,1));
+f = @(x) cos(2.*pi.*x);
 xn(1) = 0;
 xn(end) = 1;
 yn = f(xn);
-f = @(x) cos(2.*pi.*x);
 sigmas = calculo_s2(xn, yn);
 
 function s2 = s2(sigmas, x, xn, yn)
+    %
+    %   Calcula la función s2 en un punto x
+    %
+    %   Inputs 
+    %       sigmas: calculados con calculo_s2
+    %       x: punto a evaluar
+    %       xn: coordenadas x de los puntos
+    %       yn: coordenadas y de los puntos
+    %
+    %   Outputs
+    %       s2: valor de s2 en x 
+
+    % busca en qué intervalo está
     nodes = find(xn>x);
     if x == 1 
         lower = length(xn)-1;
@@ -33,6 +56,7 @@ function s2 = s2(sigmas, x, xn, yn)
         lower = nodes(1)-1;
     end
     
+    % y procede a hacer la función
     s1 = sigmas(lower);
     s2 = sigmas(lower+1);
     y1 = yn(lower);
@@ -40,34 +64,59 @@ function s2 = s2(sigmas, x, xn, yn)
     x2 = xn(lower + 1);
     s2 = y1 + s1.*(x - x1) + ((s2 - s1)/(2 * (x2 - x1)))*(x-x1)^2;
 end
-
-% s2(sigmas, 1, xn, yn)
+figure;
 xx = linspace(0,1);
 yy = arrayfun(@(x) s2(sigmas, x, xn, yn), xx);
-plot(xx, f(xx))
+plot(xx, f(xx),'', 'DisplayName', 'f(x) = cos(2\pi x)')
 hold on;
-plot(xx, yy)
+plot(xx, yy, 'r--', 'DisplayName', 'Aproximación s2')
 hold off;
+legend;
+title('Aproximación s2 usando nodos arbitrarios');
+xlabel('x');
+ylabel('Valor de la función');
 
-figure(2)
+figure;
 plot(xx, abs(f(xx) - yy))
 disp(max(abs(f(xx) - yy)))
+title('Error s2 de cos(2\pi x) usando nodos arbitrarios');
+xlabel('x');
+ylabel('Valor de la función');
 
 xn = linspace(0,1,m+1);
 yn = f(xn);
 sigmas = calculo_s2(xn, yn);
 yy = arrayfun(@(x) s2(sigmas, x, xn, yn), xx);
-figure(3)
-plot(xx, f(xx))
+figure;
+plot(xx, f(xx),'', 'DisplayName', 'f(x) = cos(2\pi x)')
 hold on;
-plot(xx, yy)
+plot(xx, yy, 'r--', 'DisplayName', 'Aproximación s2')
 hold off;
+legend;
+title('Aproximación s2 usando nodos equisdistantes');
+xlabel('x');
+ylabel('Valor de la función');
 
-figure(4)
+figure;
 plot(xx, abs(f(xx) - yy))
 disp(max(abs(f(xx) - yy)))
+title('Error s2 de cos(2\pi x) usando nodos equisdistantes');
+xlabel('x');
+ylabel('Valor de la función');
 
 function s2 = der_s2(sigmas, x, xn)
+    %
+    %   Función de la derivada explícita de s2
+    %
+    %   Inputs 
+    %       sigmas: calculados con calculo_s2
+    %       x: punto a evaluar
+    %       xn: coordenadas x de los puntos
+    %
+    %   Outputs
+    %       s2: valor de s2 en x 
+
+    % Mismo principio de búsqueda
     nodes = find(xn>x);
     if x == 1 
         lower = length(xn)-1;
@@ -75,6 +124,7 @@ function s2 = der_s2(sigmas, x, xn)
         lower = nodes(1)-1;
     end
     
+    % Pero diferente fórmula, no ocupamos yn
     s1 = sigmas(lower);
     s2 = sigmas(lower+1);
     x1 = xn(lower);
@@ -84,12 +134,19 @@ end
 
 derf = @(x) -2.*pi.*sin(2.*pi.*x);
 yy = arrayfun(@(x) der_s2(sigmas, x, xn), xx);
-figure(5)
-plot(xx, derf(xx))
+figure;
+plot(xx, derf(xx),'', 'DisplayName', 'derf(x) = -2\pi sin(2\pi x)')
 hold on;
-plot(xx, yy)
+plot(xx, yy, 'r--', 'DisplayName', 'Aproximación der_s2')
 hold off;
+legend;
+title('Derivada s2 aproximada usando nodos equisdistantes');
+xlabel('x');
+ylabel('Valor de la función');
 
-figure(6)
+figure;
 plot(xx, abs(derf(xx) - yy))
 disp(max(abs(derf(xx) - yy)))
+title('Error derivada s2 aprox de cos(2\pi x) usando nodos equisdistantes');
+xlabel('x');
+ylabel('Valor de la función');
